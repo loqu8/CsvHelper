b9440172e6fefce74e88882bd4558b1995c4255c
Tim Uy <tim@loqu8.com>
2013-12-23 14:15:50 -0800

    attempts to fix trailing \r

--- src/RegexHelper.Tests/RegexParserTests.cs
+++ src/RegexHelper.Tests/RegexParserTests.cs
@@ -14,7 +14,7 @@ namespace RegexHelper.Tests
     [TestClass]
     public class RegexParserTests
     {
-        private const string CEDictPattern = @"([^\s]+)\s+([^\s]+)\s+\[([^\]]+)\]\s+(.*)$";
+        private const string CEDictPattern = @"([^\s]+)\s+([^\s]+)\s+\[([^\]]+)\]\s+([^\r^\n]*)\r?";
         
         [TestMethod]
         public void CEDictParseTest()
@@ -36,17 +36,29 @@ namespace RegexHelper.Tests
                 writer.Flush();
                 stream.Position = 0;
 
-                var row = parser.Read();
+                var row = parser.Read();            // there is a problem here where the /r is picked up
                 Assert.IsNotNull(row);
                 Assert.AreEqual(4, row.Length);
+                Assert.AreEqual("中國", row[0]);
+                Assert.AreEqual("中国", row[1]);
+                Assert.AreEqual("Zhong1 guo2", row[2]);
+                Assert.AreEqual("/China/Middle Kingdom/", row[3]);
 
                 row = parser.Read();
                 Assert.IsNotNull(row);
                 Assert.AreEqual(4, row.Length);
+                Assert.AreEqual("珍・奧斯汀", row[0]);
+                Assert.AreEqual("珍・奥斯汀", row[1]);
+                Assert.AreEqual("Zhen1 · Ao4 si1 ting1", row[2]);
+                Assert.AreEqual("/Jane Austen (1775-1817), English novelist/", row[3]);
 
                 row = parser.Read();
                 Assert.IsNotNull(row);
                 Assert.AreEqual(4, row.Length);
+                Assert.AreEqual("人為財死，鳥為食亡", row[0]);
+                Assert.AreEqual("人为财死，鸟为食亡", row[1]);
+                Assert.AreEqual("ren2 wei4 cai2 si3 , niao3 wei4 shi2 wang2", row[2]);
+                Assert.AreEqual("/Human beings die in pursuit of wealth, and birds die in pursuit of food/…/", row[3]);
 
                 row = parser.Read();
                 Assert.IsNotNull(row);
